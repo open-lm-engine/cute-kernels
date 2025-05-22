c1edd0e3345f5c5c19f8c13a75503658db79e16a
Mayank Mishra <mayank31398@gmail.com>
2025-05-22 02:22:31 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/fused_swiglu/__init__.py
+++ cute_kernels/kernels/fused_swiglu/__init__.py
@@ -11,20 +11,34 @@ from .triton_implementation import fused_swiglu_triton
 class _FusedSwiglu_Cute(torch.autograd.Function):
     @staticmethod
     def forward(
-        ctx, x: torch.Tensor, up_weight: torch.Tensor, gate_weight: torch.Tensor, down_weight: torch.Tensor
+        ctx,
+        x: torch.Tensor,
+        gate_weight: torch.Tensor,
+        up_weight: torch.Tensor,
+        down_weight: torch.Tensor,
+        memory_efficient: bool,
     ) -> torch.Tensor:
+        intermediate_state = torch.empty(x.size(0), up_weight.size(0))
+        if memory_efficient:
+            pass
+
         output = torch.zeros_like(x, dtype=torch.float32 if x.dtype == torch.bfloat16 else x.dtype)
+
         fused_swiglu_triton(x=x, gate_weight=gate_weight, up_weight=up_weight, down_weight=down_weight, output=output)
 
-        output = output.type_as(x)
+        ctx.save_for_backward(up_weight, gate_weight, down_weight)
 
-        return output
+        return output.type_as(x)
 
     @staticmethod
     def backward(ctx, grad_output: torch.Tensor) -> tuple[torch.Tensor | None]: ...
 
 
 def fused_swiglu_cute(
-    x: torch.Tensor, up_weight: torch.Tensor, gate_weight: torch.Tensor, down_weight: torch.Tensor
+    x: torch.Tensor,
+    gate_weight: torch.Tensor,
+    up_weight: torch.Tensor,
+    down_weight: torch.Tensor,
+    memory_efficient: bool = False,
 ) -> torch.Tensor:
-    return _FusedSwiglu_Cute.apply(x, up_weight, gate_weight, down_weight)
+    return _FusedSwiglu_Cute.apply(x, gate_weight, up_weight, down_weight, memory_efficient)
