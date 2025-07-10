606ed35e9a5582ccff744761faa27b66a14b845c
Mayank Mishra <mayank31398@gmail.com>
2025-07-10 07:14:39 +0530

    runner
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- a.py
+++ a.py
@@ -12,3 +12,42 @@ B = torch.empty(8, 768, 1024, dtype=torch.int32).random_(-2, 2).to(dtype=torch.b
 C = torch.empty(8, 1024, 1024, dtype=torch.int32).random_(-2, 2).to(dtype=torch.bfloat16).cuda()
 
 bmm_cute(A=A, B=B, C=C, kernel_backend=KernelBackend.cuda)
+
+# # Create and permute tensor A/B/C
+# def create_and_permute_tensor(l, mode0, mode1, is_mode0_major, dtype):
+#     # is_mode0_major: (l, mode1, mode0) -> (mode0, mode1, l)
+#     # else: (l, mode0, mode1) -> (mode0, mode1, l)
+#     shape = (l, mode1, mode0) if is_mode0_major else (l, mode0, mode1)
+#     permute_order = (2, 1, 0) if is_mode0_major else (1, 2, 0)
+
+#     return (
+#         torch.empty(*shape, dtype=torch.int32)
+#         .random_(-2, 2)
+#         .to(dtype=dtype)
+#         .permute(permute_order)
+#         .cuda()
+#     )
+
+# L = 8
+# M = 1024
+# K = 768
+# N = 1024
+
+# a_major = "m"
+# b_major = "n"
+
+# a = create_and_permute_tensor(
+#     L, M, K, a_major == "k", torch.bfloat16
+# )
+# b = create_and_permute_tensor(
+#     L, N, K, b_major == "k", torch.bfloat16
+# )
+
+# print(a.size(), a.stride())
+# print(b.size(), b.stride())
+
+# A = A.permute(1, 2, 0)
+# print(A.size(), A.stride())
+
+# B = B.permute(2, 1, 0)
+# print(B.size(), B.stride())
--- cute_kernels/ops/bmm/cuda_implementation/ampere.py
+++ cute_kernels/ops/bmm/cuda_implementation/ampere.py
@@ -4,13 +4,32 @@
 
 import torch
 
+import cutlass.cute as cute
 from cutlass.cute.runtime import from_dlpack
 
 
+_DTYPE_MAP = {torch.float32: cute.Float32, torch.float16: cute.Float16, torch.bfloat16: cute.BFloat16}
+
+
 def ampere_gemm(A: torch.Tensor, B: torch.Tensor, C: torch.Tensor, D: torch.Tensor) -> None:
+    A_dtype = _DTYPE_MAP[A.dtype]
+    B_dtype = _DTYPE_MAP[B.dtype]
+    C_dtype = _DTYPE_MAP[C.dtype]
+    D_dtype = _DTYPE_MAP[D.dtype]
+
     A = from_dlpack(A, assumed_align=16)
     B = from_dlpack(B, assumed_align=16)
     C = from_dlpack(C, assumed_align=16)
     D = from_dlpack(D, assumed_align=16)
+
+    A = A.mark_layout_dynamic(leading_dim=2)
+    B = B.mark_layout_dynamic(leading_dim=2)
+    C = C.mark_layout_dynamic(leading_dim=2)
+    D = D.mark_layout_dynamic(leading_dim=2)
+
+    A = A.mark_compact_shape_dynamic(mode=2, stride_order=(0, 1, 2), divisibility=128 // A_dtype.width)
+    B = B.mark_compact_shape_dynamic(mode=2, stride_order=(0, 1, 2), divisibility=128 // B_dtype.width)
+    C = C.mark_compact_shape_dynamic(mode=2, stride_order=(0, 1, 2), divisibility=128 // C_dtype.width)
+    D = D.mark_compact_shape_dynamic(mode=2, stride_order=(0, 1, 2), divisibility=128 // D_dtype.width)
 
     print(A)
