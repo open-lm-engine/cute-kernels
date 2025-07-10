4a6c6faa95bcd9dc3f9877496bc1aee3cc5c8c88
Mayank Mishra <mayank31398@gmail.com>
2025-07-10 07:26:13 +0530

    runner
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/ops/bmm/cuda_implementation/ampere.py
+++ cute_kernels/ops/bmm/cuda_implementation/ampere.py
@@ -2,13 +2,54 @@
 # Copyright (c) 2025, Mayank Mishra
 # **************************************************
 
+from __future__ import annotations
+
 import torch
 
+import cutlass
 import cutlass.cute as cute
 from cutlass.cute.runtime import from_dlpack
 
 
 _DTYPE_MAP = {torch.float32: cute.Float32, torch.float16: cute.Float16, torch.bfloat16: cute.BFloat16}
+
+
+class AmpereGemm:
+    def __init__(
+        self,
+        A_dtype: type[cutlass.Numeric],
+        B_dtype: type[cutlass.Numeric],
+        C_dtype: type[cutlass.Numeric],
+        D_dtype: type[cutlass.Numeric],
+        cta_tiler: tuple[int, int, int] = (128, 128, 32),
+        atom_layout: tuple[int, int, int] = (2, 2, 1),
+        mma_instruction_shape: tuple[int, int, int] = (16, 8, 16),
+        num_stages: int = 3,
+    ) -> AmpereGemm:
+        self.A_dtype = A_dtype
+        self.B_dtype = B_dtype
+        self.C_dtype = C_dtype
+        self.D_dtype = D_dtype
+
+        self.cta_tiler = cta_tiler
+        self.atom_layout = atom_layout
+        self.mma_instruction_shape = mma_instruction_shape
+        self.num_stages = num_stages
+
+        self.num_warps = atom_layout[0] * atom_layout[1] * atom_layout[2]
+        self.num_threads = self.num_warps * 32
+
+        assert atom_layout[2] == 1
+
+        for i in range(3):
+            assert cta_tiler[i] % (atom_layout[i] * mma_instruction_shape[i]) == 0
+
+        assert self.num_stages >= 3
+
+    @cute.jit
+    def __call__(self, mA: cute.Tensor, mB: cute.Tensor, mC: cute.Tensor, mD: cute.Tensor) -> None:
+        return
+        # A_major = Layo
 
 
 def ampere_gemm(A: torch.Tensor, B: torch.Tensor, C: torch.Tensor, D: torch.Tensor) -> None:
