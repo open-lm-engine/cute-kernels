376460f5ad80b118cdc34e27bee79ec0da1de2de
Mayank Mishra <mayank31398@gmail.com>
2025-07-10 06:20:50 +0530

    runner
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- a.py
+++ a.py
@@ -4,11 +4,11 @@
 
 import torch
 
-from cute_kernels import bmm_cute
+from cute_kernels import KernelBackend, bmm_cute
 
 
 A = torch.empty(8, 1024, 768, dtype=torch.int32).random_(-2, 2).to(dtype=torch.bfloat16).cuda()
 B = torch.empty(8, 768, 1024, dtype=torch.int32).random_(-2, 2).to(dtype=torch.bfloat16).cuda()
 C = torch.empty(8, 1024, 1024, dtype=torch.int32).random_(-2, 2).to(dtype=torch.bfloat16).cuda()
 
-bmm_cute(A=A, B=B, C=C)
+bmm_cute(A=A, B=B, C=C, kernel_backend=KernelBackend.cuda)
