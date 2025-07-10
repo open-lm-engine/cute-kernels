db71cae18b53c6d5a0c3a675e55114fa1176a953
Mayank Mishra <mayank31398@gmail.com>
2025-07-10 06:20:00 +0530

    runner
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

---
+++ a.py
@@ -0,0 +1,14 @@
+# **************************************************
+# Copyright (c) 2025, Mayank Mishra
+# **************************************************
+
+import torch
+
+from cute_kernels import bmm_cute
+
+
+A = torch.empty(8, 1024, 768, dtype=torch.int32).random_(-2, 2).to(dtype=torch.bfloat16).cuda()
+B = torch.empty(8, 768, 1024, dtype=torch.int32).random_(-2, 2).to(dtype=torch.bfloat16).cuda()
+C = torch.empty(8, 1024, 1024, dtype=torch.int32).random_(-2, 2).to(dtype=torch.bfloat16).cuda()
+
+bmm_cute(A=A, B=B, C=C)
