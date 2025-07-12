4ddb17688573967e7d8eb43267f3d317e0e51a41
Mayank Mishra <mayank31398@gmail.com>
2025-07-12 06:32:28 +0530

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

---
+++ tools/throughput_add_scalar.py
@@ -0,0 +1,48 @@
+# **************************************************
+# Copyright (c) 2025, Mayank Mishra
+# **************************************************
+
+from functools import partial
+
+import torch
+from tabulate import tabulate
+
+from cute_kernels import KernelBackend, add_scalar_cute, device_synchronize
+
+
+n = 100
+
+headers = ["dtype", "torch", "CUDA kernel", "triton kernel", "cute_dsl kernel"]
+kernels = [
+    partial(add_scalar_cute, kernel_backend=KernelBackend.torch),
+    partial(add_scalar_cute, kernel_backend=KernelBackend.cuda),
+    partial(add_scalar_cute, kernel_backend=KernelBackend.triton),
+    partial(add_scalar_cute, kernel_backend=KernelBackend.cute_dsl),
+]
+
+table = []
+
+for dtype in [torch.float32, torch.float16, torch.bfloat16]:
+    row = [str(dtype)]
+    for kernel in kernels:
+        x = torch.randn(104857600, device=torch.cuda.current_device(), dtype=dtype)
+        y = 0.42
+
+        for i in range(n):
+            z = kernel(x, y)
+
+        s = torch.cuda.Event(enable_timing=True)
+        e = torch.cuda.Event(enable_timing=True)
+
+        s.record()
+        for i in range(n):
+            z = kernel(x, y)
+        e.record()
+
+        device_synchronize()
+
+        row.append(s.elapsed_time(e) / n)
+    table.append(row)
+
+
+print(tabulate(table, headers=headers))
